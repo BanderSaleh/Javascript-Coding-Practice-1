6b895ec475a6960cc8a349830c41d8535e41d6ff
“BANDER <Bandersaleh93@gmail.com>
2021-08-31 03:05:13 -0700

    Wrote a function that takes an array and returns the most repeated number and their counter and also returns the 2nd most repeated number and it's counter

--- Javascript-Coding-Practice-1.js
+++ Javascript-Coding-Practice-1.js
@@ -1,34 +1,28 @@
+// Write a function which accepts an array of integers and returns in a new two element array the two integers in the input that appear most frequently.
 
-let ASCII_SIZE = 256;
-function getMaxOccuringChar(str) {
+// majority_element_top_two([3,3,1,2,1,1,4,4,4,4]); ==> [4,1]
 
-  // Create array to keep the count of individual
-  // characters and initialize the array as 0
-  let count = new Array(ASCII_SIZE);
-  for (let i = 0; i < ASCII_SIZE; i++) {
-    count[i] = 0;
-  }
 
-  // Construct character count array from the input
-  // string.
-  let len = str.length;
-  for (let i = 0; i < len; i++) {
-    count[str[i].charCodeAt(0)] += 1;
-  }
-  let max = -1; // Initialize max count
-  let result = ' '; // Initialize result
 
-  // Traversing through the string and maintaining
-  // the count of each character
-  for (let i = 0; i < len; i++) {
-    if (max < count[str[i].charCodeAt(0)]) {
-      max = count[str[i].charCodeAt(0)];
-      result = str[i];
-    }
-  }
-  return result;
-}
+function majority_element_top_two(array) {
+  c = {}; //  counters
+  s = []; // sortable  array
 
-// Driver Method
-let str = "helloworld";
-console.log("Max occurring character is ", getMaxOccuringChar(str));
+  for (var i = 0; i < array.length; i++) {
+    c[array[i]] = c[array[i]] || 0; //initialize
+    c[array[i]]++;
+  } // count occurrences
+
+  for (var key in c) {
+    s.push([key, c[key]])
+  } // build sortable array from counters
+
+  s.sort(function (a, b) { return b[1] - a[1]; });
+  var firstMode = s[0][0];
+  var secondMode = s[1][1];
+  return [firstMode, secondMode];
+  // return the sorted array s
+};
+
+console.log(majority_element_top_two([3, 3, 1, 2, 1, 1, 4, 4, 4, 4]));
+
--- Ruby_Practice_1.rb
+++ Ruby_Practice_1.rb
@@ -1,7 +1,38 @@
-def maximumOccurringCharacter(text)
-  puts text
 
+def majority_element_top_two(numbers)
+  number = {}
+  number_count = Hash.new(0)
+  numbers.each {|number| number_count[number] += 1}
+  number_count.sort_by { |pet,number| number}.last[0]
+  puts number[0]
 end
 
 
-maximumOccurringCharacter(100)
+
+majority_element_top_two([3,3,1,2,1,1,4,4,4,4])
+  
+  
+  
+
+# Classwork (Below)
+
+def majority_element_top_two(numbers)
+  number = {}
+  number_count = Hash.new(0)
+  numbers.each {|number| number_count[number] += 1}
+  number_count.sort_by { |pet,number| number}.last[0]
+  
+end
+
+
+
+majority_element_top_two([3,3,1,2,1,1,4,4,4,4])
+  
+  
+  
+  
+  
+
+
+  
+
