648120f9205b6568efa7423d91cd607ae5d96411
“BANDER <Bandersaleh93@gmail.com>
2021-09-01 12:42:37 -0700

    Added a Function that solves a question in Ruby and Javascript

--- Ruby_Practice_1.rb
+++ Ruby_Practice_1.rb
@@ -1,38 +1,5 @@
-
-def majority_element_top_two(numbers)
-  number = {}
-  number_count = Hash.new(0)
-  numbers.each {|number| number_count[number] += 1}
-  number_count.sort_by { |pet,number| number}.last[0]
-  puts number[0]
+def majority_element_top_two(array)
+  Hash[array.group_by {|e| e}.map {|k,v| [k, v.length]}.sort_by {|array| array[1]}.reverse[0..1]]
 end
 
-
-
-majority_element_top_two([3,3,1,2,1,1,4,4,4,4])
-  
-  
-  
-
-# Classwork (Below)
-
-def majority_element_top_two(numbers)
-  number = {}
-  number_count = Hash.new(0)
-  numbers.each {|number| number_count[number] += 1}
-  number_count.sort_by { |pet,number| number}.last[0]
-  
-end
-
-
-
-majority_element_top_two([3,3,1,2,1,1,4,4,4,4])
-  
-  
-  
-  
-  
-
-
-  
-
+puts majority_element_top_two([3, 3, 1, 2, 1, 1, 4, 4, 4, 4])
